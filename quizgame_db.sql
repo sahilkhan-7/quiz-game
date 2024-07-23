c42a9064abb1eb0e8d4a641947d134533078cccd
Sahil Khan <khansahil09296@gmail.com>
2024-07-23 18:44:24 +0530

    SQL query to create database

--- quizgame_db.sql
+++ quizgame_db.sql
@@ -4,15 +4,15 @@ CREATE DATABASE quizgame;
 
 USE quizgame;
 
-CREATE TABLE quizdata(
-    sno INT AUTO_INCREMENT PRIMARY KEY, 
-    question VARCHAR(255), 
-    optionA VARCHAR(100),
-    optionB VARCHAR(100),
-    optionC VARCHAR(100),
-    optionD VARCHAR(100),
-    correct_ans VARCHAR(1)
-)
+-- CREATE TABLE quizdata(
+--     sno INT AUTO_INCREMENT PRIMARY KEY, 
+--     question VARCHAR(255), 
+--     optionA VARCHAR(100),
+--     optionB VARCHAR(100),
+--     optionC VARCHAR(100),
+--     optionD VARCHAR(100),
+--     correct_ans VARCHAR(1)
+-- )
 
 DROP TABLE quizdata;
 DROP TABLE login_info;
