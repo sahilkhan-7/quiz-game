5b2a53d3ad18959a0920c8038f9ce24285b3ca6e
Sahil Khan <khansahil09296@gmail.com>
2024-08-15 12:36:13 +0530

    MySQL Configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+# MySQL Configuration
+# Change the values to your MySQL credentials
+mysql = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'your_password',
+    'database': 'your_database_name'
+}
